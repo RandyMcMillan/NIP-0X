6d74ff1e26cbebc7106efc8fdb40051e349de263
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-07 20:23:55 -0500

    interactive-window.py: working

--- interactive-window.py
+++ interactive-window.py
@@ -11,9 +11,17 @@ layout = [
           [sg.Text("password_salt")],
           [sg.Input(key='-INPUT2-')],
           [sg.Text("index")],
-          [sg.Text(size=(64,1), key='-OUTPUT1-')],
-          [sg.Text(size=(64,1), key='-OUTPUT2-')],
-          [sg.Text(size=(64,1), key='-OUTPUT3-')],
+          [sg.Input(key='-INPUT3-')],
+
+          [sg.Text(size=(64,1), key='-BASE-')],
+          [sg.Text(size=(64,1), key='-BASEOUTPUT-')],
+
+          [sg.Text(size=(64,1), key='-BASEPW-')],
+          [sg.Text(size=(64,1), key='-BASEPWOUTPUT-')],
+
+          [sg.Text(size=(64,1), key='-BASEPWIDX-')],
+          [sg.Text(size=(64,1), key='-BASEPWIDXOUTPUT-')],
+
           [sg.Button('Ok'), sg.Button('Quit')]
           ]
 
@@ -26,12 +34,21 @@ while True:
     # See if user wants to quit or window was closed
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
-    hashed_base = hashlib.sha256(str(values['-INPUT1-']).encode('utf-8')).hexdigest()
-    window['-OUTPUT1-'].update('base_entropy:' + values['-INPUT1-'] + "")
-    window['-OUTPUT2-'].update(hashed_base)
-    hashed_base_pw = hashlib.sha256(str(values['-INPUT1-']).encode('utf-8') \
+    hashed_base = hashlib.sha256( \
+    str(values['-INPUT1-']).encode('utf-8')).hexdigest()
+    # window['-OUTPUT1-'].update('base_entropy:' + values['-INPUT1-'] + "")
+    window['-BASEOUTPUT-'].update(hashed_base)
+
+    hashed_base_pw = hashlib.sha256( \
+    str(values['-INPUT1-']).encode('utf-8') \
     +str(values['-INPUT2-']).encode('utf-8')).hexdigest()
-    window['-OUTPUT3-'].update(hashed_base_pw)
+    window['-BASEPWOUTPUT-'].update(hashed_base_pw)
+
+    hashed_base_pw_idx = hashlib.sha256( \
+    str(values['-INPUT1-']).encode('utf-8') \
+    +str(values['-INPUT2-']).encode('utf-8') \
+    +str(values['-INPUT3-']).encode('utf-8')).hexdigest()
+    window['-BASEPWIDXOUTPUT-'].update(hashed_base_pw_idx)
 
 # Finish up by removing from the screen
 window.close()
