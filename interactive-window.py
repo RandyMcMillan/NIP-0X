efe510f612750dec4e0951c7d012d11e0d6639c2
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-07 19:46:31 -0500

    interactive-window.py: base_entropy_pw -OUTPUT2-

--- interactive-window.py
+++ interactive-window.py
@@ -13,6 +13,7 @@ layout = [
           [sg.Text("index")],
           [sg.Text(size=(64,1), key='-OUTPUT1-')],
           [sg.Text(size=(64,1), key='-OUTPUT2-')],
+          [sg.Text(size=(64,1), key='-OUTPUT3-')],
           [sg.Button('Ok'), sg.Button('Quit')]
           ]
 
@@ -25,9 +26,12 @@ while True:
     # See if user wants to quit or window was closed
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
-    hashed_string = hashlib.sha256(str(values['-INPUT1-']).encode('utf-8')).hexdigest()
-    window['-OUTPUT1-'].update('Hello ' + values['-INPUT1-'] + "! Thanks for trying PySimpleGUI")
-    window['-OUTPUT2-'].update(hashed_string)
+    hashed_base = hashlib.sha256(str(values['-INPUT1-']).encode('utf-8')).hexdigest()
+    window['-OUTPUT1-'].update('base_entropy:' + values['-INPUT1-'] + "")
+    window['-OUTPUT2-'].update(hashed_base)
+    hashed_base_pw = hashlib.sha256(str(values['-INPUT1-']).encode('utf-8') \
+    +str(values['-INPUT2-']).encode('utf-8')).hexdigest()
+    window['-OUTPUT3-'].update(hashed_base_pw)
 
 # Finish up by removing from the screen
 window.close()
